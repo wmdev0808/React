bbf9ae11e5f4401b5953502eaebeed68ab3c2930
Paul <wmdev0808@gmail.com>
2022-09-12 14:45:19 -0400

    -- Implements lazy-loading using React.lazy and Suspense element -- Sets up Firebase for deployment -- Updates .gitignore -- Updates README.md

--- challenge_017/README.md
+++ challenge_017/README.md
@@ -1,6 +1,5 @@
-# Multi-page SPA with React Router
+# Multi-page SPA with React Router V6
 
-- React Router v6
 - Migrate from v5 to v6
   - Upgrade all `<Switch>` elements with `<Routes>`
   - `<Route children>` from the v5 app changed to `<Route element>`
@@ -22,3 +21,42 @@
   - When using a nested config, routes with `children` should render an `<Outlet>` in order to render their child routes.
   - Use `useNavigate` instead of `useHistory`
   - `<Prompt>` is not currently supported
+
+# Deployment using Firebase
+
+## Deployment Steps
+
+- Test code
+- Optimize code
+- Build app for production
+- Upload production code to server
+- Configure server
+
+## Deployment
+
+### Build for production
+
+- $ npm run build
+
+### Firebase hosting
+
+- Step 1: Install the Firebase CLI
+  ```
+  $ npm install -g firebase-tools
+  ```
+- Step 2: Initialize your project
+  ```
+  $ firebase init hosting
+  ```
+- Step 3: Deploy to your site
+  ```
+  $ firebase deploy --only hosting
+  ```
+
+### Cancel Firebase hosting
+
+- Step 1: Stops serving Firebase Hosting traffic for the active Firebase project.
+  ```
+  $ firebase hosting:disable
+  ```
+- Step 2: Delete hosting deployment in release history of the Firebase project by clicking on Delete context menu item of the release
--- challenge_017/src/App.tsx
+++ challenge_017/src/App.tsx
@@ -1,34 +1,45 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes, Navigate, Link } from "react-router-dom";
 
-import AllQuotes from "./pages/AllQuotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
 import Layout from "./components/layout/Layout";
-import Comments from "./components/comments/Comments";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = lazy(() => import("./pages/QuoteDetail"));
+const AllQuotes = lazy(() => import("./pages/AllQuotes"));
+const Comments = lazy(() => import("./components/comments/Comments"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="quotes" />} />
-        <Route path="quotes" element={<AllQuotes />} />
-        <Route path="quotes/:quoteId" element={<QuoteDetail />}>
-          <Route
-            path=""
-            element={
-              <div className="centered">
-                <Link className="btn--flat" to={`comments`}>
-                  Load Comments
-                </Link>
-              </div>
-            }
-          />
-          <Route path="comments" element={<Comments />} />
-        </Route>
-        <Route path="new-quote" element={<NewQuote />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate replace to="quotes" />} />
+          <Route path="quotes" element={<AllQuotes />} />
+          <Route path="quotes/:quoteId" element={<QuoteDetail />}>
+            <Route
+              path=""
+              element={
+                <div className="centered">
+                  <Link className="btn--flat" to={`comments`}>
+                    Load Comments
+                  </Link>
+                </div>
+              }
+            />
+            <Route path="comments" element={<Comments />} />
+          </Route>
+          <Route path="new-quote" element={<NewQuote />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
