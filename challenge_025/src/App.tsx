e897fc5296785fa0c873e3b65b8890585da5e900
Paul <wmdev0808@gmail.com>
2023-02-03 06:25:21 -0800

    -- Install react-router-dom v6.4 or later -- Define loader function to fetch data separately -- Setup routes, specifying the loader for a specific route -- Load data into the route component using `useLoaderData` hook

--- challenge_025/src/App.tsx
+++ challenge_025/src/App.tsx
@@ -1,27 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
 import BlogLayout from "./pages/BlogLayout";
-import BlogPostsPage from "./pages/BlogPosts";
+import BlogPostsPage, { loader as blogPostsLoader } from "./pages/BlogPosts";
 import NewPostPage from "./pages/NewPost";
 import PostDetailPage from "./pages/PostDetail";
-import RootLayout from "./components/RootLayout";
+import RootLayout from "./pages/RootLayout";
 import WelcomePage from "./pages/Welcome";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<WelcomePage />} />
+      <Route path="blog" element={<BlogLayout />}>
+        <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
+        <Route path=":id" element={<PostDetailPage />} />
+      </Route>
+      <Route path="blog/new" element={<NewPostPage />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/blog" element={<BlogLayout />}>
-            <Route index element={<BlogPostsPage />} />
-            <Route path=":id" element={<PostDetailPage />} />
-          </Route>
-          <Route path="/blog/new" element={<NewPostPage />} />
-        </Routes>
-      </RootLayout>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- challenge_025/src/pages/BlogPosts.tsx
+++ challenge_025/src/pages/BlogPosts.tsx
@@ -1,37 +1,22 @@
-import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 import Posts from "../components/Posts";
 import Post from "../types/Post";
 import { getPosts } from "../util/api";
 
 function BlogPostsPage() {
-  const [error, setError] = useState<string>();
-  const [posts, setPosts] = useState<Post[]>();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function loadPosts() {
-      setIsLoading(true);
-      try {
-        const posts = await getPosts<Post>();
-        setPosts(posts);
-      } catch (err: any) {
-        setError(err.message);
-      }
-      setIsLoading(false);
-    }
-
-    loadPosts();
-  }, []);
+  const loaderData = useLoaderData();
 
   return (
     <>
       <h1>Our Blog Posts</h1>
-      {isLoading && <p>Loading posts...</p>}
-      {error && <p>{error}</p>}
-      {!error && posts && <Posts blogPosts={posts} />}
+      <Posts blogPosts={loaderData as Post[]} />
     </>
   );
 }
 
 export default BlogPostsPage;
+
+export function loader<Post>() {
+  return getPosts<Post>();
+}
