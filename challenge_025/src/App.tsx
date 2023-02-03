af92bae99418eafd7000706fd712346aaf3a3168
Paul <wmdev0808@gmail.com>
2023-02-03 07:03:02 -0800

    -- Add loader function to PostDetail page -- Set errorElement prop of top-level Route for error handling

--- challenge_025/src/App.tsx
+++ challenge_025/src/App.tsx
@@ -7,18 +7,23 @@ import {
 
 import BlogLayout from "./pages/BlogLayout";
 import BlogPostsPage, { loader as blogPostsLoader } from "./pages/BlogPosts";
+import ErrorPage from "./pages/Error";
 import NewPostPage from "./pages/NewPost";
-import PostDetailPage from "./pages/PostDetail";
+import PostDetailPage, { loader as blogPostLoader } from "./pages/PostDetail";
 import RootLayout from "./pages/RootLayout";
 import WelcomePage from "./pages/Welcome";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
+    <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
       <Route index element={<WelcomePage />} />
       <Route path="blog" element={<BlogLayout />}>
         <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
-        <Route path=":id" element={<PostDetailPage />} />
+        <Route
+          path=":id"
+          element={<PostDetailPage />}
+          loader={blogPostLoader}          
+        />
       </Route>
       <Route path="blog/new" element={<NewPostPage />} />
     </Route>
--- challenge_025/src/pages/PostDetail.tsx
+++ challenge_025/src/pages/PostDetail.tsx
@@ -1,40 +1,23 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { type Params, useLoaderData } from "react-router-dom";
 
 import BlogPost from "../components/BlogPost";
 import Post from "../types/Post";
 import { getPost } from "../util/api";
 
 function PostDetailPage() {
-  const [error, setError] = useState<Error>();
-  const [post, setPost] = useState<Post>();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const params = useParams();
-  const { id } = params;
-
-  useEffect(() => {
-    async function loadPost() {
-      setIsLoading(true);
-      try {
-        const post = await getPost<Post>(id);
-        setPost(post);
-      } catch (err: any) {
-        setError(err.message);
-      }
-      setIsLoading(false);
-    }
-
-    loadPost();
-  }, [id]);
+  const postData = useLoaderData() as Post;
 
   return (
     <>
-      {isLoading && <p>Loading post...</p>}
-      {error && <p>{error.message}</p>}
-      {!error && post && <BlogPost title={post.title} text={post.body} />}
+      <BlogPost title={postData.title} text={postData.body} />
     </>
   );
 }
 
 export default PostDetailPage;
+
+export function loader({ params }: { params: Params<"id"> }) {
+  const postId = params.id;
+
+  return getPost<Post>(postId);
+}
